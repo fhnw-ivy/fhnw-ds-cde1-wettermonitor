3f3dd53ca8425bb8a9460514a5a6e85a91ef5607
Noah <thenoahleuenberger@gmail.com>
2022-11-03 19:25:22 +0100

    Added: retry on failure on init

--- src/weather_repository.py
+++ src/weather_repository.py
@@ -1,8 +1,53 @@
 import datetime
 import enum
 import os
+import time
 
 import weather_data as wd
+
+config = wd.Config()
+
+
+def init():
+    config.db_host = os.environ.get("INFLUXDB_HOST") if os.environ.get("INFLUXDB_HOST") else "localhost"
+    config.db_port = int(os.environ.get("INFLUXDB_PORT")) if os.environ.get("INFLUXDB_PORT") else 8086
+    print(f"DB config: host {config.db_host}, port {config.db_port}")
+
+    try:
+        wd.connect_db(config)
+        print("DB connected")
+
+        wd.import_csv_file(config=config, file_name="./data/messwerte_mythenquai_2022.csv",
+                           station="mythenquai")
+        print("CSV 'mythenquai' imported.")
+
+        wd.import_csv_file(config=config, file_name="./data/messwerte_tiefenbrunnen_2022.csv",
+                           station="tiefenbrunnen")
+        print("CSV 'tiefenbrunnen' imported.")
+
+        wd.import_latest_data(config, periodic_read=False)
+        print("Latest data imported.")
+    except:
+        print("Weather repo init failed")
+
+        print("Retrying in 3s...")
+        time.sleep(3)
+
+        init()
+
+
+def import_latest_data_periodic():
+    print("Periodic read starting")
+    wd.import_latest_data(config, periodic_read=True)
+
+
+def get_all_entries(station: str, start_time: datetime, stop_time: datetime = None):
+    try:
+        return wd.get_entries(config=config, station=station, start_time=start_time, stop_time=stop_time)
+    except:
+        print("get_all_entries entries failed")
+
+    return None
 
 
 class Measurement(enum.Enum):
@@ -19,37 +64,3 @@ class Measurement(enum.Enum):
     Wind_speed_avg_10min = "wind_speed_avg_10min"
     Wind_chill = "windchill"
     Radiation = "global_radiation"
-
-
-config = wd.Config()
-
-
-def init():
-    config.db_host = os.environ.get("INFLUXDB_HOST") if os.environ.get("INFLUXDB_HOST") else "localhost"
-    config.db_port = int(os.environ.get("INFLUXDB_PORT")) if os.environ.get("INFLUXDB_PORT") else 8086
-    wd.connect_db(config)
-
-    try:
-        wd.import_csv_file(config=config, file_name="./data/messwerte_mythenquai_2022.csv",
-                           station="mythenquai")
-
-        wd.import_csv_file(config=config, file_name="./data/messwerte_tiefenbrunnen_2022.csv",
-                           station="tiefenbrunnen")
-    except:
-        raise Exception("CSV import failed")
-
-    wd.import_latest_data(config, periodic_read=False)
-
-
-def import_latest_data_periodic():
-    print("Periodic read starting")
-    wd.import_latest_data(config, periodic_read=True)
-
-
-def get_all_entries(station: str, start_time: datetime, stop_time: datetime = None):
-    try:
-        return wd.get_entries(config=config, station=station, start_time=start_time, stop_time=stop_time)
-    except:
-        print("get_all_entries entries failed")
-
-    return None
