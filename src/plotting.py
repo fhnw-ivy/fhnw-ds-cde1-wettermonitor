e7baab5253a876f3abef99003da110e1b5492d61
Dominik Filliger <dominik.filliger@students.fhnw.ch>
2022-11-18 08:26:37 +0100

    added plotly

--- src/plotting.py
+++ src/plotting.py
@@ -2,7 +2,7 @@ import datetime
 
 import weather_repository as wr
 import seaborn as sns
-
+import plotly.express as px
 sns.set_theme()
 
 
@@ -15,12 +15,16 @@ def generate_last_week_plots():
     data = wr.run_query(data_query)
 
     plot = sns.lineplot(data=data, x="time", y=wr.Measurement.Air_temp.value)
+    plot = px.line(data, x="time", y=wr.Measurement.Air_temp.value)
+
     save_plot(plot, "air_temp_7days")
+
 
 
 def save_plot(plot, plot_name):
     try:
-        plot.get_figure().savefig(f'./static/{plot_name}.png', dpi=500, bbox_inches='tight')
+        plot.savefig(f"/static/plots/{plot_name}.png")
+        # plot.get_figure().savefig(f'./static/{plot_name}.png', dpi=500, bbox_inches='tight')
     except Exception as e:
         print("save_plot failed.")
         print(e)
