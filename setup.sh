b2b00b4e23731de443891b80909c9e7464e3a3d4
Noah Leuenberger <39002725+noaahh@users.noreply.github.com>
2022-12-19 20:31:48 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
+# Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
 
+# Clone repository
 git clone https://github.com/fhnw-ivy/fhnw-ds-cde1-wettermonitor.git
 cd fhnw-ds-cde1-wettermonitor || exit
 
 cp example.env .env
-
 docker compose up -d
 
+# Configure autostart
 cp -fr autostart /etc/xdg/lxsession/LXDE-pi/autostart
+
+# Reboot
+shutdown -r +2 "Server is going to reboot in two minutes"
