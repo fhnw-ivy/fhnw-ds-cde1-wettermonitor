caae4e337399283701dfc306ad9481bc02a6810d
Noah <thenoahleuenberger@gmail.com>
2022-12-30 17:24:38 +0100

    Added: echos in install script

--- install.sh
+++ install.sh
@@ -1,15 +1,25 @@
 # Install Docker
+echo "Installing Docker"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
+echo "Docker installed"
 
 # Clone repository
+echo "Cloning repository"
 git clone https://github.com/fhnw-ivy/fhnw-ds-cde1-wettermonitor.git
 cd fhnw-ds-cde1-wettermonitor || exit
-
-cp example.env .env
-docker compose up -d
+echo "Repository cloned"
 
 # Configure autostart
+echo "Configuring autostart"
 cp -fr autostart /etc/xdg/lxsession/LXDE-pi/autostart
+echo "Autostart configured"
 
-sudo shutdown -r
+# Configure Docker
+echo "Configuring Docker"
+cp example.env .env
+docker compose up -d
+echo "Docker configured"
+
+echo "Installation finished. Rebooting device..."
+sudo shutdown -r now
