0e9e82264569421695a158ff4ed1d4c5fa7b1a50
Noah <thenoahleuenberger@gmail.com>
2023-01-11 20:02:11 +0100

    Added echos to utility scripts

--- download_csvs.sh
+++ download_csvs.sh
@@ -1,11 +1,14 @@
 {
   year=$(date +%Y)
 
+  echo "Downloading CSVs for $year"
   wget https://data.stadt-zuerich.ch/dataset/sid_wapo_wetterstationen/download/messwerte_mythenquai_$year.csv
   wget https://data.stadt-zuerich.ch/dataset/sid_wapo_wetterstationen/download/messwerte_tiefenbrunnen_$year.csv
+  echo "Done"
 
   mv -f messwerte_mythenquai_$year.csv ./csv/messwerte_mythenquai.csv
   mv -f messwerte_tiefenbrunnen_$year.csv ./csv/messwerte_tiefenbrunnen.csv
+  echo "Moved CSVs to ./csv"
 } || {
     echo "Download of CSV files failed. Relying on fallback data."
 }
